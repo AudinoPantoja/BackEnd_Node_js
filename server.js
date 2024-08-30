7f64ec762ccd0f348483aeb2d2b847c059718549
AudinoPantoja <pantojabel@gmail.com>
2024-08-29 21:49:35 -0500

    Modificacion server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   // Establecer el código de estado y el tipo de contenido de la respuesta
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola, Mundo!\n'); // Enviar la respuesta
+  res.end('Hola, Mundo verificacion!\n'); // Enviar la respuesta
 });
 
 // Asignar un puerto y hacer que el servidor escuche en ese puerto
