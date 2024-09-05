5e953cfa533d73f9176ab486b4cbd5dcc58a3384
AudinoPantoja <pantojabel@gmail.com>
2024-09-04 20:32:57 -0500

    Cambio server_response_json

--- server_response_json.js
+++ server_response_json.js
@@ -1,9 +1,32 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ message: 'Hola, Mundo!', status: 'success' }));
+
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.statusCode = 200;
+      res.end(JSON.stringify({ message: 'Hola, Mundo!', status: 'success' }));
+    } else if (req.url === '/about') {
+      res.statusCode = 200;
+      res.end(JSON.stringify({ message: 'Acerca de', info: 'Este es un servidor básico en Node.js' }));
+    } else if (req.url === '/data') {
+      res.statusCode = 200;
+      res.end(JSON.stringify({
+        users: [
+          { name: 'Andres', age: 22, country: 'México' },
+          { name: 'Yaddy', age: 32, country: 'Colombia' },
+          { name: 'Alberto', age: 27, country: 'Francia' }
+        ]
+      }));
+    } else {
+      res.statusCode = 404;
+      res.end(JSON.stringify({ error: 'Ruta no encontrada', status: 'fail' }));
+    }
+  } else if (req.method === 'POST') {
+    res.statusCode = 200;
+    res.end(JSON.stringify({ message: 'Solicitud POST recibida', status: 'success' }));
+  } 
 });
 
 const port = 3000;
