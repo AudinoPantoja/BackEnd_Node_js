76faa4c111ee95b6d5fb5ce84917ec295bf1e597
AudinoPantoja <pantojabel@gmail.com>
2024-09-18 23:24:56 -0500

    Clase TypeScript

---
+++ TypeScript/build/types/examplesTypes.js
@@ -0,0 +1,5 @@
+let userName = 'Peter';
+let age = 30;
+let isAdmin = true;
+console.log(userName, 'userName', '\n', age, 'age', '\n', isAdmin, 'isAdmin');
+//# sourceMappingURL=examplesTypes.js.map
---
+++ TypeScript/src/types/examplesTypes.ts
@@ -0,0 +1,5 @@
+let userName: string = 'Peter';
+let age: number = 30;
+let isAdmin: boolean = true;
+console.log (userName, 'userName','\n', age, 'age','\n', isAdmin, 'isAdmin');
+
